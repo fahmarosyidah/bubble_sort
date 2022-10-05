3eaa6e9e09e8444bb4e72b3f3f571d9d8ec868f5
fahmarosyidah <fahmarosyi@gmail.com>
2022-10-05 17:29:37 +0700

    Fungsi yang menerima angka untuk menentukan banyaknya data yang disimpan pada array

--- Program.cs
+++ Program.cs
@@ -13,7 +13,21 @@ namespace bubble_sort
         //Fungsi atau method untuk menerima masukan
         public void read()
         {
-           
+            //Menerima angka untuk menentukan banyaknya data yang disimpan pada array
+            while (true)
+            {
+                Console.Write("Masukkan banyaknya elemen pada Array: ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\nArray dapat mempunyai maksimal 20 elemen.\n");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("Masukkan elemen array ");
+            Console.WriteLine("---------------------------");
         }
 
 
