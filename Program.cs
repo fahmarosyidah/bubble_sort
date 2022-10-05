6b8bd9601c9dbb8ba36824a7932928dcd7f31eb2
fahmarosyidah <fahmarosyi@gmail.com>
2022-10-05 17:21:13 +0700

    Deklarasi variabel int untuk menyimpan banyaknya data pada array

--- Program.cs
+++ Program.cs
@@ -7,6 +7,9 @@ namespace bubble_sort
         //Deklarasi array int dengan ukuran 20
         private int[] a = new int[20];
 
+        //Deklarasi variabel int untuk menyimpan banyaknya data pada array
+        private int n;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
